9e6e098b430d3dd741b9259a5a0f7eb9073afe07
Stefan Shivarov <71455791+StefanShivarov@users.noreply.github.com>
2021-05-09 13:03:04 +0300

    Doc 14

---
+++ 13, 14/14/Circle.java
@@ -0,0 +1,34 @@
+public class Circle{
+	int radius;
+	public Circle(int radius) {
+		this.radius = radius;
+	}
+	public String toString() {
+		return "Radius: "+this.radius;
+	}
+}
+
+
+
+
+
+
+//
+//public class Circle implements Comparable<Circle>{
+//
+//	public int radius;
+//	
+//	public Circle(int radius) {
+//		this.radius = radius;
+//	}
+//
+//	@Override
+//	public int compareTo(Circle arg0) {
+//		
+//		return this.radius - arg0.radius;
+//	}
+//	
+//	public String toString() {
+//		return "Radius: "+this.radius;
+//	}
+//}
